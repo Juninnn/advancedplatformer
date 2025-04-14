30255612e73561b4a2542543aa66b3aae2924368
JuninnDev_ <andrezindsytb@gmail.com>
2025-04-14 17:57:05 -0300

    mudanças na textura do player e no player

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,12 +1,12 @@
 extends KinematicBody2D
 class_name Player
 
+#Onready player_sprite       
 onready var player_sprite: Sprite = get_node("Texture")
 
 var velocity: Vector2 
 
 var jump_count: int = 0
-
 var landing: bool = false
 var attacking: bool = false
 var defending: bool = false
@@ -21,32 +21,28 @@ func _physics_process(delta: float):
 	horizontal_movement_env()
 	vertical_movement_env()
 	actions_env()
-	
+
 	gravity(delta)
 	velocity = move_and_slide(velocity, Vector2.UP)
-	
-	if player_sprite != null:
-		player_sprite.animate(velocity)
-	else:
-		print("Erro: player_sprite não está inicializado.")
+	player_sprite.animate(velocity)
 
 func horizontal_movement_env() -> void:
 	var input_direction: float = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	if can_track_input == false or attacking:
+	if not can_track_input or attacking:
 		velocity.x = 0
 		return
-		
-	velocity.x  = input_direction * speed
-	
+
+	velocity.x = input_direction * speed
+
 func vertical_movement_env() -> void:
 	if is_on_floor():
 		jump_count = 0
+
 	var jump_condition: bool = can_track_input and not attacking
-	if Input.is_action_just_pressed("jump") and jump_count < 2 and jump_condition:
-		print("Jumping!")  # Debugging line
+	if Input.is_action_just_pressed("ui_select") and jump_count < 2 and jump_condition:
 		jump_count += 1
-		velocity.y = -jump_speed  # Ensure this is negative for upward movement
-''
+		velocity.y = jump_speed
+
 func actions_env() -> void:
 	attack()
 	crouch()
@@ -57,27 +53,26 @@ func attack() -> void:
 	if Input.is_action_just_pressed("attack") and attack_condition and is_on_floor():
 		attacking = true
 		player_sprite.normal_attack = true
-	
+
 func crouch() -> void:
-	if Input.is_action_just_pressed("crouch") and is_on_floor() and not defending:
+	if Input.is_action_pressed("crouch") and is_on_floor() and not defending:
 		crouching = true
-		can_track_input = false;
-	elif not defending:
-		crouching = false;
-		can_track_input = true;
-		player_sprite.crouching_off = true
+		can_track_input = false
+	else:
+		if not defending:
+			crouching = false
+			can_track_input = true
 
 func defense() -> void:
-	if Input.is_action_just_pressed("defend") and is_on_floor() and not crouching:
+	if Input.is_action_pressed("defense") and is_on_floor() and not crouching:
 		defending = true
 		can_track_input = false
-	elif not crouching:
-		defending = false
-		can_track_input = true
-		player_sprite.shield_off = true
+	else:
+		if not crouching:
+			defending = false
+			can_track_input = true
 
 func gravity(delta: float) -> void:
 	velocity.y += delta * player_gravity
 	if velocity.y >= player_gravity:
 		velocity.y = player_gravity
-

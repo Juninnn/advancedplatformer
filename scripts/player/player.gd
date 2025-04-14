d8ee9d9c23b8c02b16caa488a13a3e1c799e1ffc
JuninnDev_ <andrezindsytb@gmail.com>
2025-04-14 18:16:28 -0300

    alterações feitas para resolver um BUG na animação do player

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -39,7 +39,7 @@ func vertical_movement_env() -> void:
 		jump_count = 0
 
 	var jump_condition: bool = can_track_input and not attacking
-	if Input.is_action_just_pressed("ui_select") and jump_count < 2 and jump_condition:
+	if Input.is_action_just_pressed("jump") and jump_count < 2 and jump_condition:
 		jump_count += 1
 		velocity.y = jump_speed
 
@@ -64,7 +64,7 @@ func crouch() -> void:
 			can_track_input = true
 
 func defense() -> void:
-	if Input.is_action_pressed("defense") and is_on_floor() and not crouching:
+	if Input.is_action_pressed("defend") and is_on_floor() and not crouching:
 		defending = true
 		can_track_input = false
 	else:
--- scripts/player/player_texture.gd
+++ scripts/player/player_texture.gd
@@ -3,8 +3,6 @@ class_name PlayerTexture
 
 var suffix: String = "_right"
 var normal_attack: bool = false
-var shield_off: bool = false
-var crouching_off: bool = false
 
 export(NodePath) var animation_path
 export(NodePath) var player_path
@@ -12,20 +10,21 @@ export(NodePath) var player_path
 onready var animation = get_node(animation_path) if animation_path != null else null
 onready var player = get_node(player_path) if player_path != null else null
 
+func _ready():
+	if animation and not animation.is_connected("animation_finished", self, "on_animation_finished"):
+		animation.connect("animation_finished", self, "on_animation_finished")
+
 func animate(direction: Vector2) -> void:
 	verify_position(direction)
-	if player != null:
-		if player.attacking or player.defending or player.crouching:
-			action_behavior()
-		elif direction.y != 0:
-			vertical_behavior(direction)
-		elif player.landing == true:
-			animation.play("landing")
-			player.set_physics_process(false)
-		else:
-			horizontal_behavior(direction)
+	if player.attacking or player.defending or player.crouching:
+		action_behavior()
+	elif direction.y != 0:
+		vertical_behavior(direction)
+	elif player.landing == true:
+		animation.play("landing")
+		player.set_physics_process(false)
 	else:
-		print("O nó player não foi encontrado.")
+		horizontal_behavior(direction)
 
 func verify_position(direction: Vector2) -> void:
 	if direction.x > 0:
@@ -38,41 +37,37 @@ func verify_position(direction: Vector2) -> void:
 func action_behavior() -> void:
 	if player.attacking and normal_attack:
 		animation.play("attack" + suffix)
-	elif player.defending and not shield_off:
-		animation.play("shield")
-		shield_off = false
-	elif player.crouching and not crouching_off:
-		animation.play("crouch")
-		crouching_off = false
-		
+	elif player.defending:
+		if animation.current_animation != "shield":
+			if animation.has_animation("shield"):
+				animation.play("shield")
+				animation.stop()
+				animation.seek(animation.get_animation("shield").length, true)
+	elif player.crouching:
+		if animation.current_animation != "crouch":
+			if animation.has_animation("crouch"):
+				animation.play("crouch")
+				animation.stop()
+				animation.seek(animation.get_animation("crouch").length, true)
 func vertical_behavior(direction: Vector2) -> void:
 	if direction.y > 0:
 		player.landing = true
 		animation.play("fall")
 	elif direction.y < 0:
 		animation.play("jump")
-	
+
 func horizontal_behavior(direction: Vector2) -> void:
 	if direction.x != 0:
-		print("Running...")  # Debugging line
 		animation.play("run")
 	else:
-		print("Idle...")  # Debugging line
-		
-		
-		
+		animation.play("idle")
+
 func on_animation_finished(anim_name):
 	match anim_name:
 		"landing":
 			player.landing = false
 			player.set_physics_process(true)
-			
-		"attack_left":
+
+		"attack_left", "attack_right":
 			normal_attack = false
 			player.attacking = false
-			
-		"attack_right":
-			normal_attack = false
-			player.attacking = false
-		
-		
